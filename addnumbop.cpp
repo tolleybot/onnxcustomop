9651bfe197a32fc64f518021b43fc752b4ac758d
root <root@ip-172-31-22-93.ec2.internal>
2024-02-10 18:58:15 +0000

    working docker containers

---
+++ addnumbop.cpp
@@ -0,0 +1,42 @@
+#include <onnx/onnx_pb.h>
+#include "onnxruntime_config.h"
+#include "core/framework/op_kernel.h"
+
+namespace onnxruntime {
+
+class AddTwoNumbersOp : public onnxruntime::OpKernel {
+public:
+  explicit AddTwoNumbersOp(const OpKernelInfo& info) : OpKernel(info) {}
+
+  Status Compute(OpKernelContext* context) const override {
+    // Get the input tensors
+    const Tensor* input_tensor_0 = context->Input<Tensor>(0);
+    const Tensor* input_tensor_1 = context->Input<Tensor>(1);
+
+    // Check that the input tensors have the same shape
+    ORT_ENFORCE(input_tensor_0->Shape() == input_tensor_1->Shape(),
+                "Input tensors must have the same shape");
+
+    // Create the output tensor
+    Tensor* output_tensor = context->Output(0, input_tensor_0->Shape());
+
+    // Compute the output values
+    for (size_t i = 0; i < input_tensor_0->Shape().Size(); i++) {
+      output_tensor->MutableData<float>()[i] = input_tensor_0->Data<float>()[i] + input_tensor_1->Data<float>()[i];
+    }
+
+    return Status::OK();
+  }
+};
+
+// Register the kernel for the custom operator
+ONNX_OPERATOR_KERNEL_EX(
+    AddTwoNumbers,  // The name of the operator
+    kMSDomain,  // The domain of the operator (use kMSDomain for custom operators)
+    1,  // The version of the operator
+    kCpuExecutionProvider,  // The execution provider for the operator
+    KernelDefBuilder()
+        .TypeConstraint("T", DataTypeImpl::GetTensorType<float>()),  // The data type constraint for the operator
+    AddTwoNumbersOp);  // The class that implements the operator
+
+}
---
+++ cmake/onnxruntimeConfig.cmake
@@ -0,0 +1,28 @@
+# This will define the following variables:
+#   onnxruntime_FOUND        -- True if the system has the onnxruntime library
+#   onnxruntime_INCLUDE_DIRS -- The include directories for onnxruntime
+#   onnxruntime_LIBRARIES    -- Libraries to link against
+#   onnxruntime_CXX_FLAGS    -- Additional (required) compiler flags
+
+include(FindPackageHandleStandardArgs)
+
+# Assume we are in <install-prefix>/share/cmake/onnxruntime/onnxruntimeConfig.cmake
+get_filename_component(CMAKE_CURRENT_LIST_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+#get_filename_component(onnxruntime_INSTALL_PREFIX "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
+
+set(onnxruntime_INCLUDE_DIRS "/lib" "/usr/include" "/usr/local/include" "/onnxruntime/onnxruntime/" "/onnxruntime/include/onnxruntime"  "/onnxruntime/include/onnxruntime/core" "/onnxruntime/include/onnxruntime/core/session")
+set(onnxruntime_LIBRARIES onnxruntime)
+set(onnxruntime_CXX_FLAGS "") # no flags needed
+
+
+find_library(onnxruntime_LIBRARY onnxruntime
+    PATHS "/usr/lib" "/usr/local/lib"
+)
+
+
+add_library(onnxruntime SHARED IMPORTED)
+set_property(TARGET onnxruntime PROPERTY IMPORTED_LOCATION "${onnxruntime_LIBRARY}")
+set_property(TARGET onnxruntime PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${onnxruntime_INCLUDE_DIRS}")
+set_property(TARGET onnxruntime PROPERTY INTERFACE_COMPILE_OPTIONS "${onnxruntime_CXX_FLAGS}")
+
+find_package_handle_standard_args(onnxruntime DEFAULT_MSG onnxruntime_LIBRARY onnxruntime_INCLUDE_DIRS)
